f49c87392d1e7b72a8a60f26639679d5f1c9f19f
khalidnadish <w2.nadish@gmail.com>
2022-06-10 15:36:28 +0300

    x123333333222eeee

--- helpeer/config.js
+++ helpeer/config.js
@@ -5,11 +5,16 @@ export const cnf = {
   port: process.env.PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectTimeout: 30000,
   connectionLimit: 10,
-  avatarUrl: process.env.AVATAR_URL,
+  base_url: process.env.BASE_URL + process.env.PORT + "/",
+  avatarUrl: process.env.BASE_URL + process.env.PORT + "/images/avatar/",
+
+  // AVATAR_URL=http://localhost:3001/images/avatar/
+  // BASE_URL=http://localhost:3001/
 };
 
 // export const avatarUrl = process.env.AVATAR_URL;
