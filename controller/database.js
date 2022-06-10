7e586e1fa0107eff79848e83e1a9b9074d0625d1
khalidnadish <w2.nadish@gmail.com>
2022-06-10 17:26:35 +0300

    x123333333222cccl

--- controller/database.js
+++ controller/database.js
@@ -2,18 +2,24 @@ import db from "mysql2";
 import { cnf } from "../helpeer/config.js";
 
 const dataBase = db.createPool({
-  host: cnf.host,
-  user: cnf.user,
-  database: cnf.dataBase,
-  password: cnf.password,
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b439f2e618e26d",
+  database: "heroku_eaa8275903daac8",
+  password: "a68ca059",
   connectTimeout: 30000,
   connectionLimit: 10,
 });
 export default dataBase;
 
-// host: "localhost",
-//   user: "nadish",
-//   database: "nadish_site",
-//   password: "Leno_1972",
+// mysql://b439f2e618e26d:a68ca059@us-cdbr-east-05.cleardb.net/heroku_eaa8275903daac8?reconnect=true
+
+// LOCAL CONFIG FOR DATABASE
+
+// const dataBase = db.createPool({
+//   host: cnf.host,
+//   user: cnf.user,
+//   database: cnf.dataBase,
+//   password: cnf.password,
 //   connectTimeout: 30000,
 //   connectionLimit: 10,
+// });
